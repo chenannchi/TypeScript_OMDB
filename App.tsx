344d8e9bc653a2219f9d760fc5ca3b8722907cb9
chenannchi <anchichenusa@gmail.com>
2023-05-30 16:28:25 +0800

    first commit

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  Text,
+  View,
+  StatusBar
+} from 'react-native';
+import HomeScreen from './src/screens/HomeScreen';
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        {/* Landing Page Goes Here */}
+        <HomeScreen />
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+export default App;
