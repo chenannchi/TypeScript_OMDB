5bede493d90d77a488b018f4004ccb3f0ddce3ce
chenannchi <anchichenusa@gmail.com>
2023-05-31 17:02:11 +0800

    add and show the result of the search

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -3,26 +3,32 @@ import React from 'react'
 import Banner from '../components/Banner'
 import SearchField from '../components/SearchField'
 import { useState } from 'react'
-
+import Result from '../components/Result'
 
 export type MovieResultType = {
-
+  Title: string;
+  Poster: string;
+  Rated: string;
+  Plot: string;
 }
 
 const HomeScreen = () => {
-  const [movieResults, setMovieResults] = useState<MovieResultType | null>(null)
+  const [movieResult, setMovieResult] = useState<MovieResultType | null>(null)
 
-  const handleSearch =async (searchString:string):Promise<void> => {
-    const result = await fetch(`https://omdbapi.com/?t=${searchString}&apikey=2c0bcb9f`).then(data=>data.json())
-    setMovieResults(result)
-    console.log(movieResults)
+  const handleSearch = async (searchString: string): Promise<void> => {
+    const result = await fetch(`https://omdbapi.com/?t=${searchString}&apikey=2c0bcb9f`).then(data => data.json())
+    setMovieResult(result)
+    console.log(movieResult)
   }
 
   return (
     <SafeAreaView className='min-h-screen min-w-screen'>
       <Banner />
-
       <SearchField onSubmit={handleSearch} />
+      <View className='flex-row justify-center'>
+        {movieResult?.Title && <Result movie={movieResult} />}
+        {/* <Result /> */}
+      </View>
     </SafeAreaView>
   )
 }
